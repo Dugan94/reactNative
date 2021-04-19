504ec2f743d446fe28a12dad9088fe513ba1e1ab
Brandon Dugan <duganbrandon225@gmail.com>
2021-04-19 05:53:05 -0700

    favorites

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,54 +1,63 @@
-import React, { Component } from 'react';
-import { View, FlatList } from 'react-native';
-import { Tile } from 'react-native-elements';
-import { connect } from 'react-redux';
-import { baseURL } from '../shared/baseURL';
-import Loading from './LoadingComponent';
+import React, { Component } from "react";
+import { View, FlatList, Text } from "react-native";
+import { Tile } from "react-native-elements";
+import { connect } from "react-redux";
+import { baseURL } from "../shared/baseURL";
+import Loading from "./LoadingComponent";
 
-const mapStateToProps = state => {
-    return {
-        campsites: state.campsites,
-    };
+const mapStateToProps = (state) => {
+  return {
+    campsites: state.campsites,
+  };
 };
 
 class Directory extends Component {
+  static navigationOptions = {
+    title: "Directory",
+  };
 
-    static navigationOptions = {
-        title: 'Directory'
+  render() {
+    const { navigate } = this.props.navigation;
+    const renderDirectoryItem = ({ item }) => {
+      return (
+        <Tile
+          title={item.name}
+          caption={item.description}
+          featured
+          onPress={() => navigate("CampsiteInfo", { campsiteId: item.id })}
+          imageSrc={{ uri: baseURL + item.image }}
+        />
+      );
+    };
+
+    if (this.props.campsites.isLoading) {
+      return <Loading />;
     }
-
-    render() {
-        const { navigate } = this.props.navigation;
-        const renderDirectoryItem = ({item}) => {
-            return (
-                <Tile
-                    title={item.name}
-                    caption={item.description}
-                    featured
-                    onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
-                    imageSrc={{uri: baseURL + item.image }}
-                />
-            );
-        };
-
-        if (this.props.campsites.isLoading) {
-            return <Loading />;
-        }
-        if (this.props.campsites.errMess) {
-            return (
-                <View>
-                    <Text>{props.campsites.errMess}</Text>
-                </View>
-            );
-        }
-        return (
-            <FlatList
-                data={this.props.campsites.campsites}
-                renderItem={renderDirectoryItem}
-                keyExtractor={item => item.id.toString()}
-            />
-        );
+    if (this.props.campsites.errMess) {
+      return (
+        <View>
+          <Text>{props.campsites.errMess}</Text>
+        </View>
+      );
     }
+    return (
+      <FlatList
+        data={this.props.campsites.campsites}
+        renderItem={renderDirectoryItem}
+        keyExtractor={(item) => item.id.toString()}
+      />
+    );
+  }
 }
 
 export default connect(mapStateToProps)(Directory);
+
+
+
+
+
+
+
+
+
+
--- redux/comments.js
+++ redux/comments.js
@@ -9,7 +9,6 @@ export const comments = (state = { errMess: null, comments: [] }, action) => {
       return { ...state, errMess: action.payload };
 
     case ActionTypes.ADD_COMMENT:
-      //const newComment = { ...action.payload, id: state.comments.length };
       const newComment = action.payload;
       newComment.id = state.comments.length;
       return { ...state, comments: state.comments.concat(newComment) };
